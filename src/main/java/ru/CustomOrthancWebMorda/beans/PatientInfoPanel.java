3e03ed74976df1c6ef977b35da0839dfc6446545
id-05 <id-05@mail.ru>
2021-05-01 23:45:31 +0900

    find linkbutton

--- src/main/java/ru/CustomOrthancWebMorda/beans/PatientInfoPanel.java
+++ src/main/java/ru/CustomOrthancWebMorda/beans/PatientInfoPanel.java
@@ -4,11 +4,13 @@ import org.primefaces.PrimeFaces;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.util.Map;
 
 @ManagedBean(name = "patientInfoPanel", eager = true)
-@ViewScoped
+@RequestScoped
 public class PatientInfoPanel {
 
     public static String patientId;
@@ -21,6 +23,18 @@ public class PatientInfoPanel {
         this.patientId = patientId;
     }
 
+
+    @PostConstruct
+    public void init(){
+        System.out.println("click open patient");
+        //patientId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("parentPatientID");
+        //System.out.println(patientId);
+        Map<String,String> params;
+        params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        patientId = params.get("parentPatientID");
+        System.out.println(patientId);
+        PrimeFaces.current().ajax().update("patientinfo");
+    }
 
 
     public void clickOpenPatient(){
