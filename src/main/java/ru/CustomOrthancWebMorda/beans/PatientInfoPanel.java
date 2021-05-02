e60de524cbd96de352adc00072aa0da14183bc52
id-05 <id-05@mail.ru>
2021-05-02 19:01:55 +0900

    add custom css

--- src/main/java/ru/CustomOrthancWebMorda/beans/PatientInfoPanel.java
+++ src/main/java/ru/CustomOrthancWebMorda/beans/PatientInfoPanel.java
@@ -4,13 +4,11 @@ import org.primefaces.PrimeFaces;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import java.util.Map;
 
 @ManagedBean(name = "patientInfoPanel", eager = true)
-@RequestScoped
+@ViewScoped
 public class PatientInfoPanel {
 
     public static String patientId;
@@ -24,18 +22,6 @@ public class PatientInfoPanel {
     }
 
 
-    @PostConstruct
-    public void init(){
-        System.out.println("click open patient");
-        //patientId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("parentPatientID");
-        //System.out.println(patientId);
-        Map<String,String> params;
-        params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        patientId = params.get("parentPatientID");
-        System.out.println(patientId);
-        PrimeFaces.current().ajax().update("patientinfo");
-    }
-
 
     public void clickOpenPatient(){
         System.out.println("click open patient");
@@ -45,7 +31,7 @@ public class PatientInfoPanel {
     }
 
     public void printParam(){
-        patientId = "987654321";
+        //patientId = "987654321";
         System.out.println(patientId);
     }
 
