af33b7343e7593700ef726ec3e84090eab6a3184
id-05 <id-05@mail.ru>
2023-02-05 00:00:15 +0900

    dicom creator functional start

---
+++ src/main/java/ru/bitServer/beans/DicomCreatorBean.java
@@ -0,0 +1,10 @@
+package ru.bitServer.beans;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "dicomcreatorBean")
+@ViewScoped
+public class DicomCreatorBean {
+}
